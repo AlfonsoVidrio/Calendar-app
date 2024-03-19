d8f2dd0884b0b18cf54c33b430809a6c2a9de47a
AlfonsoVidrio <71527311+ALPH-CLOUD@users.noreply.github.com>
2024-03-18 22:21:39 -0600

    se agrego el startSavingEvent para guardar los eventos

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -26,7 +26,7 @@ Modal.setAppElement('#root');
 export const CalendarModal = () => {
 
     const { isDateModalOpen, closeDateModal } = useUiStore();
-    const { activeEvent } = useCalendarStore();
+    const { activeEvent, startSavingEvent } = useCalendarStore();
 
     const [ formSubmitted, setFormSubmitted ] = useState(false);
 
@@ -74,7 +74,7 @@ export const CalendarModal = () => {
     }
 
 
-    const onSubmit = ( event ) => {
+    const onSubmit = async ( event ) => {
         event.preventDefault();
         setFormSubmitted(true);
 
@@ -88,8 +88,10 @@ export const CalendarModal = () => {
         if( formValues.title.length <= 0) return;
         console.log(formValues);
 
+        await startSavingEvent( formValues );
         // cerrar modal
-        // remover errores en pantalla
+        onCloseModal();
+        setFormSubmitted(false);
     }
 
 return (
