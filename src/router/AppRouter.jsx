08f043c20899d72be69526e288f0523592dda765
AlfonsoVidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2024-03-26 20:44:22 -0600

    se agrego un helper para obtener las variables de entorno

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,11 +2,14 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../auth'
 import { CalendarPage } from '../calendar';
 import { RegisterPage } from '../auth/pages/RegisterPage';
+import { getEnvVariables } from '../helpers';
 
 export const AppRouter = () => {
 
     const authStatus = 'authenticated' //'not-authenticated';
 
+    console.log(getEnvVariables());
+    
     return (
         <Routes>
 
